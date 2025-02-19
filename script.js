9a99172d0d86cacb4c4571aa07935cb81f1b01da
kolya944994 <kolya944.994@mail.ru>
2025-02-19 19:53:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-var button = document.querySelector('.body')
-console.log('ghb')
+var button = document.querySelector('.additional-menu__button')
+var brand = document.querySelector('.brand')
+
+button.addEventListener('click', function () {
+	if (brand.style.display === 'grid')
+		(brand.style.display = 'none'),
+			(button.textContent = 'Показать все'),
+			button.classList.toggle('additional-menu__button_click')
+	else
+		(brand.style.display = 'grid'),
+			(button.textContent = 'Скрыть'),
+			button.classList.remove('additional-menu__button_click')
+})
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+})
