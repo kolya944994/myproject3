3ddff10b108d4e5e3ab6f18b3f5b466eccdae66f
kolya944994 <kolya944.994@mail.ru>
2025-02-20 20:51:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 var button = document.querySelector('.additional-menu__button')
-var brand = document.querySelector('.brand')
+var brand = document.querySelectorAll('.brand__list_display-js')
 
-button.addEventListener('click', function () {
-	if (brand.style.display === 'grid')
-		(brand.style.display = 'none'),
-			(button.textContent = 'Показать все'),
-			button.classList.toggle('additional-menu__button_click')
-	else
-		(brand.style.display = 'grid'),
-			(button.textContent = 'Скрыть'),
-			button.classList.remove('additional-menu__button_click')
-})
-
+var addClicker = function (brands) {
+	button.addEventListener('click', function () {
+		if (brands.style.display == 'none') {
+			;(brands.style.display = 'flex'),
+				(button.textContent = 'Скрыть'),
+				button.classList.toggle('additional-menu__button_click')
+		} else {
+			;(brands.style.display = 'none'),
+				(button.textContent = 'Показать все'),
+				button.classList.remove('additional-menu__button_click')
+		}
+	})
+}
+for (var i = 0; i < brand.length; i++) {
+	addClicker(brand[i])
+}
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	direction: 'horizontal',
+	slidesPerView: 1.2,
+	spaceBetween: 16,
 	loop: true,
 
 	// If we need pagination
