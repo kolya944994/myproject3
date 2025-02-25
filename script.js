f25ccfaceaea1435a4ed47d2fdee5477123a748c
kolya944994 <kolya944.994@mail.ru>
2025-02-25 21:23:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const button = document.querySelector('.additional-menu__button')
-const brand = document.querySelectorAll('.swiper-slide')
+const brand = document.querySelector('.swiper-wrapper')
 
 button.addEventListener('click', function () {
-	brand.forEach(brand => {
-		button.classList.toggle('additional-menu__button_click')
-		brand.classList.toggle('swiper-slide_display-tablejs')
-		brand.classList.toggle('swiper-slide_display-dekstopjs')
-
-		if (brand.classList.contains('swiper-slide_display-tablejs')) {
-			button.textContent = 'Скрыть'
-		} else {
-			button.textContent = 'Показать Все'
-		}
-	})
+	button.classList.toggle('additional-menu__button_click')
+	brand.classList.toggle('swiper-wrapperjs')
+	if (brand.classList.contains('swiper-wrapperjs')) {
+		button.textContent = 'Скрыть'
+	} else {
+		button.textContent = 'Показать Все'
+	}
 })
 
 const screenWidth = window.innerWidth
